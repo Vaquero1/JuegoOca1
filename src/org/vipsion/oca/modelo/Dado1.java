8cb6cca777076154cf57e330b2fc53aa16f842c3
Vaquero <francisco.vaquero1@gmail.com>
2019-04-17 12:47:39 +0200

    2º Instantanea clase dado y tablero

---
+++ src/org/vipsion/oca/modelo/Dado1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.vipsion.oca.modelo;
+
+/**
+ *
+ * @author pisko
+ */
+public class Dado1  extends Casilla{
+    
+    public Dado1(int posicion) {
+        super(posicion);
+    }
+    
+   @Override
+   public void posaFicha(Ficha ficha) {
+        
+    }
+}
